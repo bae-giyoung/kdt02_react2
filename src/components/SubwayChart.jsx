af28c82b9c0c39c792badac23e9177622d434a9c
bae-giyoung <giyoung1012@gmail.com>
2025-07-25 10:53:27 +0900

    차트 상태 변경 처리

--- src/components/SubwayChart.jsx
+++ src/components/SubwayChart.jsx
@@ -13,6 +13,7 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js'
+
 import { Line } from 'react-chartjs-2'
 
 ChartJS.register(
@@ -35,15 +36,19 @@ export default function SubwayChart() {
 
     // 오늘 날짜
     const date = new Date();
-    const today = date.getFullYear() + ("00" + (date.getMonth() + 1)).slice(-2) + ("00" + date.getDate()).slice(-2);
+    const today = date.toISOString().slice(0, 10).replaceAll("-", "");
 
     // 셀렉트 값 변경되면 데이터 요청하기
     const handleSelect = async () => {
+        if(selRef.current.value == "") {
+            setTdata([]);
+            return;
+        }
+
         const apikey = import.meta.env.VITE_DATA_API;
         const areaCode = selRef.current.value;
         const baseUrl = "https://apis.data.go.kr/6260000/IndoorAirQuality/getIndoorAirQualityByStation?";
-        const url = `${baseUrl}serviceKey=${apikey}&pageNo=1&numOfRows=12&resultType=json&controlnumber=20250724&areaIndex=${areaCode}`;
-        //const url = `${baseUrl}serviceKey=${apikey}&pageNo=1&numOfRows=12&resultType=json&controlnumber=${today}&areaIndex=${areaCode}`;
+        const url = `${baseUrl}serviceKey=${apikey}&pageNo=1&numOfRows=12&resultType=json&controlnumber=${today}&areaIndex=${areaCode}`;
         //console.log(url, today);
 
         const resp = await fetch(url);
@@ -52,7 +57,7 @@ export default function SubwayChart() {
         
         if (contentType.includes('application/json')) {  // 자정이 넘어서 해당 일자의 실시간 측정 data가 존재하지 않아서 xml을 응답하는 경우가 있었음
         const data = await resp.json();
-        console.log(data.response);
+        //console.log(data.response);
         
             if(data.response.header.resultCode == "00") {  
                 if(data.response.body["numOfRows"] == "0") {
@@ -77,37 +82,27 @@ export default function SubwayChart() {
 
     
     // 차트 그리기
-    const drawCharts = () => {
+    const drawCharts = (stateCode) => {
+        if(stateCode != 0) {
+            setChart([]);
+            return;
+        }
+
         const chartColors = ["red", "orange", "yellow", "green", "blue", "black", "violet", "gray", "brown"];
         const labels = [...tdata].map(item => item["controlnumber"].slice(-2) + "시");
         const cate1 = tableKeys.current.filter(item => (!item.includes("no") && item !=  "co"));
         const cate2 = tableKeys.current.filter(item => (item.includes("no") || item == "co")); // 0.0대에서 왔다갔다 하는 애들은 따로 빼서 차트 하나 더 그리자!
-
         
-        /* const chData = {
-            labels,
-            datasets: tableKeys.current.map((cate, idx) => {
-                return {
-                    label: cate,
-                    data: tdata.map(data => data[cate] == "-" ? null : parseFloat(data[cate])),
-                    //spanGaps: true,
-                    borderColor: chartColors[idx],
-                    backgroundColor: 'transparent',
-                    borderDash: idx % 3 == 0 ? [6,2,1,2] : idx % 3 == 1 ? [] : [5,2,2,2],
-                    yAxisID: cate != "co2" ? 'y' : 'y1',
-                    }
-                    }),
-                    }; */
-                    
         const chData1 = {
             labels,
             datasets: cate1.map((cate, idx) => {
                 return {
                     label: cate,
                     data: tdata.map(data => data[cate] == "-" ? null : parseFloat(data[cate])),
+                    spanGaps: true,
                     borderColor: chartColors[idx],
                     backgroundColor: 'transparent',
-                    borderDash: idx % 3 == 0 ? [6,2,1,2] : idx % 3 == 1 ? [] : [5,2,2,2],
+                    borderDash: idx % 3 == 0 ? [5,2,2,2] : idx % 3 == 1 ? [] : [5,5],
                     yAxisID: cate != "co2" ? 'y' : 'y1',
                 }
             }),
@@ -119,9 +114,10 @@ export default function SubwayChart() {
                 return {
                     label: cate,
                     data: tdata.map(data => data[cate] == "-" ? null : parseFloat(data[cate])),
+                    spanGaps: true,
                     borderColor: chartColors[idx],
                     backgroundColor: 'transparent',
-                    borderDash: idx % 3 == 0 ? [6,2,1,2] : idx % 3 == 1 ? [] : [5,2,2,2],
+                    borderDash: idx % 3 == 0 ? [5,2,2,2] : idx % 3 == 1 ? [] : [5,5],
                     yAxisID: cate != "co" ? 'y' : 'y1',
                 }
             }),
@@ -169,31 +165,35 @@ export default function SubwayChart() {
                     },
                 }
         )});
-        //const chOptions2 = Object.assign({}, chOptions1);
-        //chOptions2.plugins.title.text = "지하철 실내 공기질 정보2 (질소 화합물)";
         
-        //console.log(chData);
-        //setChart(<Line key={"chData"} options={chOptions} data={chData} />);
         setChart(
             [
                 chData1.datasets.map((item => item.data.reduce((prev, curr) => prev + curr))).reduce((prev, curr) => prev +curr) != 0 &&
-                <div className="w-full flex-1/2 shrink-0 flex justify-center"><Line key={"chData1"} options={chOptions1} data={chData1} className="w-full max-w-full" /></div>, 
+                <div key={"chData1"} className="w-full flex-1/2 shrink-0 flex justify-center"><Line options={chOptions1} data={chData1} className="w-full max-w-full" /></div>, 
                 
                 chData2.datasets.map((item => item.data.reduce((prev, curr) => prev + curr))).reduce((prev, curr) => prev +curr) != 0 &&
-                <div className="w-full flex-1/2 shrink-0 flex justify-center"><Line key={"chData2"} options={chOptions2} data={chData2} className="w-full max-w-full" /></div>
+                <div key={"chData2"} className="w-full flex-1/2 shrink-0 flex justify-center"><Line options={chOptions2} data={chData2} className="w-full max-w-full" /></div>
             ]
         );
     }
     
-    const makeTableUnits = (obj) => {
+    const makeTableUnits = (obj, objIdx) => {
       const tableUnits = tableKeys.current.map((item, idx) => {
           return (
           <div key={idx} className="mt-2 text-center border border-black">
-              <p className="bg-green-800 text-white font-extrabold px-1 box-border">
+              <p className={
+                objIdx % 2 == 0
+                ? "bg-green-800 text-white font-extrabold px-1 box-border"
+                : "bg-sky-900 text-white font-extrabold px-1 box-border"
+              }>
                   {scode[item]["name"]}<br />{item}
               </p>
               <p className="px-1 box-border">
-                  {obj[item]}
+                  {
+                    obj[item] != "-" 
+                    ? obj[item] + scode[item]["unit"]
+                    : "-"
+                  }
               </p>
           </div>
           )
@@ -207,12 +207,19 @@ export default function SubwayChart() {
         
         // 표 그리기
         let eachTable = null;
+        let stateCode = 0;
         
-        if(!isInit.current && tdata.length == 1 && tdata[0].includes("no-data")) {
+        if (tdata.length == 0){
+            eachTable = <div>구역을 선택해주세요</div>
+            stateCode = 1;
+
+        } else if (!isInit.current && tdata.length == 1 && tdata[0].includes("no-data")) {
             eachTable = <div>해당하는 데이터가 존재하지 않습니다.</div>
+            stateCode = 2;
 
         } else if(!isInit.current && tdata.length == 1 && tdata[0].includes("extra-error")) {
             eachTable = <div>예상치 못한 에러가 발생했습니다.</div>
+            stateCode = 3;
 
         } else {
             eachTable = tdata.map((obj, idx) =>
@@ -221,15 +228,18 @@ export default function SubwayChart() {
                         ({date.toLocaleDateString() + " " + obj["controlnumber"].slice(-2)}:00)
                     </p>
                     <div className="grid grid-cols-9">
-                        {makeTableUnits(obj)}
+                        {makeTableUnits(obj, idx)}
                     </div>
                 </div>
             );
-
-            drawCharts();
         }
-
+        
+        drawCharts(stateCode);
         setResultTable(eachTable);
+
+        /* return (()=> {
+            // 차트 여기서 무너뜨려야 할 것 같은데...?
+        }) */
     }, [tdata]);
 
     return (
